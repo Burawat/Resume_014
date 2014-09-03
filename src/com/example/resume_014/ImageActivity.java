b5506d908feaca51b29d341f95b1a803e2e40af5
Burawat <Sola_un@hotmail.com>
2014-09-03 08:49:24 +0700

    Resume

---
+++ src/com/example/resume_014/ImageActivity.java
@@ -0,0 +1,14 @@
+package com.example.resume_014;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View.OnCreateContextMenuListener;
+
+public class ImageActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.image55410014);
+	}
+}
